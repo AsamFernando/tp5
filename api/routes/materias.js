6634282f2645a700ca5c38bcd9ef97e247ea805b
AsamFernando <ferasam88@gmail.com>
2022-09-14 01:11:29 -0300

    cambios en get para mostrar todos los campos, en post para poder cargar cada campo, en put para poder modificar cada campo y en funcion findMateria

--- api/routes/materias.js
+++ api/routes/materias.js
@@ -6,7 +6,7 @@ router.get("/", (req, res) => {
   console.log("Esto es un mensaje para ver en consola");
   models.materia
     .findAll({
-      attributes: ["id", "nombre"]
+      attributes: ["id", "nombre", "id_carrera"]
     })
     .then(materias => res.send(materias))
     .catch(() => res.sendStatus(500));
@@ -14,8 +14,8 @@ router.get("/", (req, res) => {
 
 router.post("/", (req, res) => {
   models.materia
-    .create({ nombre: req.body.nombre })
-    .then(materia => res.status(201).send({ id: materia.id }))
+    .create({ nombre: req.body.nombre, id_carrera: req.body.id_carrera })
+    .then(materia => res.status(201).send({ id: materia.id}))
     .catch(error => {
       if (error == "SequelizeUniqueConstraintError: Validation error") {
         res.status(400).send('Bad request: existe otra materia con el mismo nombre')
@@ -30,7 +30,7 @@ router.post("/", (req, res) => {
 const findMateria = (id, { onSuccess, onNotFound, onError }) => {
   models.materia
     .findOne({
-      attributes: ["id", "nombre"],
+      attributes: ["id", "nombre", "id_carrera"],
       where: { id }
     })
     .then(materia => (materia ? onSuccess(materia) : onNotFound()))
@@ -48,7 +48,7 @@ router.get("/:id", (req, res) => {
 router.put("/:id", (req, res) => {
   const onSuccess = materia =>
     materia
-      .update({ nombre: req.body.nombre }, { fields: ["nombre"] })
+      .update({ nombre: req.body.nombre, id_carrera: req.body.id_carrera }, { fields: ["nombre", "id_carrera"] })
       .then(() => res.sendStatus(200))
       .catch(error => {
         if (error == "SequelizeUniqueConstraintError: Validation error") {
